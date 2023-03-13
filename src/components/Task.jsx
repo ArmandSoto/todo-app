20ddb139f4249404e9857722105848419cd179bf
ArmandSoto <armand.e.soto@gmail.com>
2023-03-13 01:36:29 -0400

    Updated handleDeleteTask and added UI TaskPanel

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,13 @@
 
 export default function Task(props){
+
+    //onClick used to be (e => {props.deleteTask(e.target)})
     
     return (
         <div className="border-2 shadow-lg rounded-lg my-2 p-4">
             <li>
                 {props.id + ". " + props.name}
-                <button className=" border-2 ml-3" onClick={(e) => { props.deleteTask(e.target) } }> Delete </button>
+                <button className=" border-2 ml-3" onClick={(e) => { props.deleteTask(props.id) } }> Delete </button>
             </li>
         </div>
     )
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,38 +3,55 @@ import Task from './Task'
 
 export default function TodoList(){
 
-    const [ tasks, setTasks ] = useState(JSON.parse(localStorage.getItem('items'))|| [])
+    const [ tasks, setTasks ] = useState(JSON.parse(localStorage.getItem('items')) || [])
     const [ currentTask, setCurrentTask ] = useState({
         name: "",
         id: "", 
     })
+    const [ taskPaneIsActive, setTaskPaneIsActive ] = useState(false)
 
 
     const taskComponents = tasks.map((task, index) => {
         return <Task 
                 key={index}
                 name={task.name}
-                id={index + 1}
+                id={task.id}
                 deleteTask={handleDeleteTask}
             />
             
     })
 
 
-useEffect(() => {   
-    localStorage.setItem('items', JSON.stringify(tasks))
-}, [tasks])
+    useEffect(() => {   
+        localStorage.setItem('items', JSON.stringify(tasks))
+    }, [tasks])
 
-
+    
     function handleSubmit(e){
         e.preventDefault()
-        addTask()       
+        addTask()
+        setCurrentTask({
+            name: "",
+            id: "",
+        })       
     }
 
-    function handleDeleteTask(task){       
+    function handleDeleteTask(taskID){    
+        
         setTasks(prevTasks => {
-            return prevTasks.splice(task.id - 1, 1)
+            
+            let taskArray = prevTasks.filter(item => item.id != taskID)
+            let taskArrayMap = taskArray.map((task, index) =>{
+                return {
+                    ...task,
+                    id: index + 1,
+                }
+            })
+                  
+            return taskArrayMap
         })
+        
+        
     }
 
     function handleChange(e){
@@ -57,24 +74,47 @@ useEffect(() => {
             }
         )
     }
+
+
+    function handleClick(){
+        setTaskPaneIsActive(prev => !prev)
+    }
     
+    function handleKeyPress(event){
+        if(event.key === 'Enter'){
+            setTaskPaneIsActive(prev => !prev)
+            
+        }
+    }
 
     return (
-        <div className="flex-column justify-center border-2 w-96">
+        <div className={"flex-column justify-center border-2 w-96"}>
             <form onSubmit={handleSubmit} className="flex space-x-4 items-center">
-                <label htmlFor="task-name">Task:</label>
-                <input className="border-2" 
-                       type="text"
-                       name="task-name" 
-                       onChange={handleChange}
-                       value={currentTask.name}
-                        
-                />
-                <button className="rounded-full h-12 w-12 bg-green-500 text-white text text-xl" > + </button>    
+                <div className={taskPaneIsActive ? "" : "hidden"}>
+                    <label htmlFor="task-name">Task:</label>
+                        <input className="border-2" 
+                            type="text"
+                            name="task-name" 
+                            onChange={handleChange}
+                            value={currentTask.name}
+                            onKeyDown={handleKeyPress}
+                            placeholder="Enter a Task..."    
+                        />
+                </div>
             </form>
-                <ul className="flex-column space-between">
-                    {taskComponents}
-                </ul>
+            
+            <div className={""}> {/* handles the background blur */}
+                <div className={"flex-column text-center"}>
+                    <div className={"flex border-2 justify-around"}>
+                        <p className={tasks.length > 0 ? "hidden" : "leading-10"}>You have no tasks currently</p>
+                        <button className="rounded-full h-12 w-12 bg-green-500 text-white text text-xl"
+                                onClick={handleClick} > + </button>    
+                    </div>
+                    <ul className="flex-column space-between">
+                            {taskComponents}
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
